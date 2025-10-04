f8f438969cb9f3c330920b384d4fb6de67ee19d2
wfybexploits <wfybexploits@gmail.com>
2025-10-04 19:27:41 -0400

    Create V4.lua

---
+++ V4.lua
@@ -0,0 +1,2 @@
+-- ⚠️ WARNING: Integrity Protected! | R🐀TWARE.EXE Universal Subplace Teleporter | Youtube.com/@WFYBExploits | Made in Mumbai, India (2025)
+loadstring(game:HttpGet("https://gist.githubusercontent.com/wfybexploits/635aaae621a6673d17b9b5e9be4f23a9/raw/2a2f6607af74dfc234db44141566bf195a894fe4/UniversalSubplaceTeleporter.lua"))()
